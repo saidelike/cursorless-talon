1d8663eeb04877f8f275437bfa8ef2e90e01491e
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-12-14 16:55:52 +0100

    temporarily disabled edit commands

--- src/andreas/edit.py
+++ src/andreas/edit.py
@@ -1,24 +1,24 @@
-from talon import Context, actions
-from ..targets.target_types import PrimitiveTarget, PrimitiveDestination
+# from talon import Context, actions
+# from ..targets.target_types import PrimitiveTarget, PrimitiveDestination
 
 
-ctx = Context()
-ctx.matches = r"""
-app: vscode
-tag: user.cursorless_in_text_editor
-"""
+# ctx = Context()
+# ctx.matches = r"""
+# app: vscode
+# tag: user.cursorless_in_text_editor
+# """
 
-target = PrimitiveTarget(None, None)
-destination = PrimitiveDestination("to", target)
+# target = PrimitiveTarget(None, None)
+# destination = PrimitiveDestination("to", target)
 
 
-@ctx.action_class("edit")
-class Actions:
-    def cut():
-        actions.user.cursorless_command("cutToClipboard", target)
+# @ctx.action_class("edit")
+# class Actions:
+#     def cut():
+#         actions.user.cursorless_command("cutToClipboard", target)
 
-    def copy():
-        actions.user.cursorless_command("copyToClipboard", target)
+#     def copy():
+#         actions.user.cursorless_command("copyToClipboard", target)
 
-    def paste():
-        actions.user.private_cursorless_paste(destination)
+#     def paste():
+#         actions.user.private_cursorless_paste(destination)
