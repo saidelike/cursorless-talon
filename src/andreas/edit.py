cbdcb375398951854446984822b96b26d3b368c4
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-12-17 15:30:31 +0100

    added expand parameter to paste

--- src/andreas/edit.py
+++ src/andreas/edit.py
@@ -1,5 +1,9 @@
 from talon import Context, actions
-from ..targets.target_types import ImplicitDestination, PrimitiveTarget
+from ..targets.target_types import (
+    ImplicitDestination,
+    PrimitiveDestination,
+    PrimitiveTarget,
+)
 
 ctx = Context()
 ctx.matches = r"""
@@ -7,16 +11,21 @@ app: vscode
 win.title: /\[Text Editor\]$/
 """
 
-target = PrimitiveTarget(None, None)
+primitive_target = PrimitiveTarget(None, None)
+primitive_destination = PrimitiveDestination("to", primitive_target)
+implicit_destination = ImplicitDestination()
 
 
 @ctx.action_class("user")
 class Actions:
     def edit_cut():
-        actions.user.cursorless_command("cutToClipboard", target)
+        actions.user.cursorless_command("cutToClipboard", primitive_target)
 
     def edit_copy():
-        actions.user.cursorless_command("copyToClipboard", target)
+        actions.user.cursorless_command("copyToClipboard", primitive_target)
 
-    def edit_paste():
-        actions.user.private_cursorless_paste(ImplicitDestination())
+    def edit_paste(expand: bool):
+        if expand:
+            actions.user.private_cursorless_paste(primitive_destination)
+        else:
+            actions.user.private_cursorless_paste(implicit_destination)
